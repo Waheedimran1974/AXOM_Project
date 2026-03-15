1483db03cfaf4b54c2c59c956c7bf101074638f2
Waheedimran1974 <drnadirakhanbds@gmail.com>
2026-03-15 07:23:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # Secure Configuration
 genai.configure(api_key=st.secrets["GEMINI_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 st.set_page_config(page_title="AXOM Global", layout="wide", page_icon="🚀")
 
